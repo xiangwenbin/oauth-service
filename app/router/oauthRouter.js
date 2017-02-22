e00f01357878c6dd2b41ef4e232133ce3f5d4d6e
xwb <xwb0128@gmail.com>
2017-02-22 18:55:48 +0800

    Misc

--- app/router/oauthRouter.js
+++ app/router/oauthRouter.js
@@ -2,7 +2,7 @@ import router from 'koa-router';
 import db from '../koa2-oauth/db';
 import Util from '../util/util';
 import log4js from '../log4js';
-import {oauth} from '../koa2-oauth';
+// import {oauth} from '../koa2-oauth';
 const log = log4js.getLogger('DEBUG');
 
 const OauthRouter = router();
